54b27c292ff310585f6ac697271d0d741c3cf2d9
LukeKedz <lukekedziora@gmail.com>
2015-06-06 12:31:30 -0400

    passed capitalize_words

--- capitalize_words.rb
+++ capitalize_words.rb
@@ -1,3 +1,19 @@
-File.open(ARGV[0]).line do |words|
+File.open(ARGV[0]).each_line do |words|
+  words = words.split(" ")
 
+  i = 1
+  words.each do |word|
+    chars = word.split(//)
+    chars[0] = chars[0].upcase
+    word = chars.join
+
+    if i < words.length
+      print word + " "
+      i += 1
+    else
+      print word
+    end
+  end
+
+  puts
 end
