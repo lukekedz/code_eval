6f6e87afbd2bd73e3fb35eaeec770ced3cfb478b
Luke Kedz <luke.kedziora@jefferson.edu>
2016-05-31 11:35:08 -0400

    slightly upd data_recovery

--- data_recovery.rb
+++ data_recovery.rb
@@ -8,9 +8,10 @@ File.open(ARGV[0]).each_line do |line|
 	sentence_count = jumbled.length
 
 	sentence_count.times do |i|
-		if !(order.include?(i)) then @missing_i = i end
+		if !(order.include?(i)) 
+			sentence[i] = jumbled[-1]
+		end
 	end
-	sentence[@missing_i] = jumbled[-1]
 
 	jumbled.each_with_index do |value, index|
 		sentence[order[index]] = value
