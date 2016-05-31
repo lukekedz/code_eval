e6bd49d8bad12dbb8505387f5aa36fb050706530
Luke Kedz <luke.kedziora@jefferson.edu>
2016-05-31 11:03:53 -0400

    solved data_recovery

---
+++ data_recovery.rb
@@ -0,0 +1,24 @@
+File.open(ARGV[0]).each_line do |line|
+	input_arr = line.split(';')
+
+	jumbled = input_arr[0].split(' ')
+	order   = input_arr[1].split(' ').map { |value| value.to_i - 1 }
+	
+	sentence = {}
+	sentence_count = jumbled.length
+
+	sentence_count.times do |i|
+		if !(order.include?(i)) then @missing_i = i end
+	end
+	sentence[@missing_i] = jumbled[-1]
+
+	jumbled.each_with_index do |value, index|
+		sentence[order[index]] = value
+	end
+
+	sentence_count.times do |i|
+		print i < sentence_count ? sentence[i] + " " : sentence[i]
+	end
+
+	puts
+end
