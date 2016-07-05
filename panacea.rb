786ab29bfd231290a3d17f7da5de6fc18a66f8fb
Luke Kedz <luke.kedziora@jefferson.edu>
2016-07-05 10:13:13 -0400

    input upd

--- panacea.rb
+++ panacea.rb
@@ -3,23 +3,17 @@ File.open(ARGV[0]).each_line do |line|
 
 	virus = 0
 	antivirus = 0
-
 	pipe = false
 
-	# [1, 2, 3, 4, 5].split(3)              # => [[1, 2], [4, 5]]
+	input_arr.each do |value|
+		if value == "|"
+			pipe = true
+			next
+		end
 
-	input_arr.split("|")
-	puts input_arr
+		if pipe == false then virus     = virus + value.to_i(16)    end
+		if pipe == true  then antivirus = antivirus + value.to_i(2) end 
+	end
 
-	# input_arr.each do |value|
-	# 	if value == "|"
-	# 		pipe = true
-	# 		next
-	# 	end
-
-	# 	if pipe == false then virus     = virus + value.to_i(16)    end
-	# 	if pipe == true  then antivirus = antivirus + value.to_i(2) end 
-	# end
-
-	# puts antivirus >= virus ? "True" : "False"
+	puts antivirus >= virus ? "True" : "False"
 end
