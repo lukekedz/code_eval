2af352c639a249bb413a191db5fc683e70987346
Luke Kedz <luke.kedziora@jefferson.edu>
2016-07-15 11:32:20 -0400

    solved compressed sequence

--- compressed_sequence.rb
+++ compressed_sequence.rb
@@ -1,4 +1,23 @@
-File.open(ARGV[0]).each_line do |nums|
+File.open(ARGV[0]).each_line do |line|
+	input = line.split(" ").map! { |str| str.to_i }
 
+	counter = 0
+	i = 0
+	input.each_with_index do |value, index|
+		while value == input[i]
+			counter += 1
+			i += 1
+		end
 
+		if counter == 0
+			next
+		else
+			print counter.to_s + " " + value.to_s
+			if (index + 1) != input.length then print " " end
+
+			counter = 0
+		end
+	end
+
+	puts
 end
