b3362cb334d1794880f12e311db8513126c7ceae
Luke Kedz <luke.kedziora@jefferson.edu>
2016-09-15 13:46:27 -0400

    solved happy numbers

--- happy_numbers.rb
+++ happy_numbers.rb
@@ -1,20 +1,22 @@
+def sum_sq(num, i)
+  if num.to_s.length > 1
+    numbers = num.to_s.split(//)
+    num = 0
+
+    numbers.each { |n| num += n.to_i ** 2 }
+  else
+    num = num ** 2
+  end
+
+  i += 1
+  if num == 1 || i > 1000
+    num
+  else
+    sum_sq(num, i)
+  end
+end
+
 File.open(ARGV[0]).each_line do |num|
-
-  num = num.to_i
-
-  if num == 1
-    p num
-  end
-
-  while num != 1
-    num = num.to_s.split(//)
-    num.map! { |str| str.to_i * str.to_i }
-    num = num.inject(:+)
-      if num == 1
-        p num
-      else
-
-      end
-  end
-
+  num = sum_sq(num.strip.to_i, 1)
+  puts num == 1 ? 1 : 0
 end
