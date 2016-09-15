fc26e7f2ca1f966d97825a6cf8dd40f5f656ef73
Luke Kedz <luke.kedziora@jefferson.edu>
2016-09-15 13:47:13 -0400

    examples

--- _examples.rb
+++ _examples.rb
@@ -1,1 +1,8 @@
-__examples.rb
+# print a space unless last element
+if (index - 1) != output.length then print " " end
+
+# examples of better input splitting
+input = line.split(" ").map! { |str| str.to_i }
+array = line.split(';')[1].split(',').map(&:strip)
+
+
