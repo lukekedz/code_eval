68ace2a47a0655c1d2599355e2cc9bd60a7454b2
Luke Kedz <luke.kedziora@jefferson.edu>
2017-01-19 12:07:06 -0500

    refactored flavius josephus

--- flavius_josephus.rb
+++ flavius_josephus.rb
@@ -1,30 +1,25 @@
 File.open(ARGV[0]).each_line do |line|
-	people_count = line.split(",")[0].to_i
-	people_array = []
-	people_count.times { |i| people_array.push i }
-
-	interval = line.split(",")[1].to_i
+	population = line.split(",")[0].to_i
+	interval   = line.split(",")[1].to_i
+	lineup     = []
+	population.times { |i| lineup.push i }
 
 	order = []
 	i = 0
 
-	people_count.times do
+	population.times do
+		# reset interval each time a person is pushed into the final order
 		loop_interval = interval
 
 		until loop_interval == 0
-			
-			if i >= people_count
-
+			if i >= population
+				# reached the end of the array, restart at lineup[0] 
 				i = 0
-
 			else
-
-				if people_array[i] != nil 
-					if loop_interval == 1 && people_array[i] != nil
+				if lineup[i] != nil 
+					if loop_interval == 1
 						order.push i
-						people_array[i] = nil
-					elsif loop_interval == 1 && people_array[i] == nil
-						loop_interval += 1
+						lineup[i] = nil
 					end
 
 					loop_interval -= 1
@@ -33,11 +28,9 @@ File.open(ARGV[0]).each_line do |line|
 				i += 1
 			end
 		end
-
-
-
 	end
 
+	# display value according to code_eval requirement
 	order.each_with_index do |value, index|
 		print value.to_s
 		if (index - 1) != order.length then print " " end
