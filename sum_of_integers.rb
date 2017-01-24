0d552ac5ca522d02f909617c0fc6773008f397f6
Luke Kedz <luke.kedziora@jefferson.edu>
2017-01-24 08:48:24 -0500

    solved moderate sum of integers

--- sum_of_integers.rb
+++ sum_of_integers.rb
@@ -1,8 +1,21 @@
-total = 0
+# https://www.codeeval.com/open_challenges/17/
 
 File.open(ARGV[0]).each_line do |line|
-  num = line.to_i
-  total += num
+	input    = line.split(",").map { |char| char.to_i }
+	sums     = []
+	temp_sum = 0
+
+	input.each_with_index do |n, index|
+		i = index
+		
+		while i < input.length
+			sums.push ( temp_sum += input[i] )
+			i += 1
+		end
+
+		temp_sum = 0
+	end
+
+	puts sums.max
 end
 
-p total
